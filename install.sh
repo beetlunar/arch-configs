f5dca2142c28747af15338c99ee0a8636e464c85
beetlunar <beetlunar@pm.me>
2025-09-16 15:28:58 +0300

    all de related configs

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ flatpak install flathub app.zen_browser.zen
 git clone https://github.com/beetlunar/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo pacman -S --needed --noconfirm hyprlock hyprpicker hyprsunset xdg-desktop-portal-hyprland hyprpolkitagent qt6-wayland qt5-wayland hypridle
+sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd otf-font-awesome xdg-desktop-portal-gtk pavucontrol base-devel unzip ranger cliphist
