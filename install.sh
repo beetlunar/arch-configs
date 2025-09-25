f5c98e55c93ac20e6d30002903c734d01dd212a3
beetlunar <beetlunar@pm.me>
2025-09-25 15:14:07 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -S --needed --noconfirm zsh nvim flatpak ghostty wl-clipboard zsh-syntax-highlighting go wofi hyprland github-cli waybar hyprpaper
+sudo pacman -S --needed --noconfirm hyprlock hyprpicker hyprsunset xdg-desktop-portal-hyprland hyprpolkitagent qt6-wayland qt5-wayland hypridle
+sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd xdg-desktop-portal-gtk pavucontrol base-devel unzip ranger cliphist
 flatpak install flathub app.zen_browser.zen
 git clone https://github.com/beetlunar/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo pacman -S --needed --noconfirm hyprlock hyprpicker hyprsunset xdg-desktop-portal-hyprland hyprpolkitagent qt6-wayland qt5-wayland hypridle
-sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd otf-font-awesome xdg-desktop-portal-gtk pavucontrol base-devel unzip ranger cliphist
