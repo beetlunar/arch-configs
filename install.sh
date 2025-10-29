0bc7e92b0fa2fe50bbaecc1c6b2ff9c9d4b76f9b
beetlunar <beetlunar@pm.me>
2025-10-29 16:45:04 +0300

    add git cred and system clipboad integration to zvm

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 sudo pacman -S --needed --noconfirm zsh nvim flatpak ghostty wl-clipboard zsh-syntax-highlighting go wofi hyprland github-cli waybar hyprpaper
 sudo pacman -S --needed --noconfirm hyprlock hyprpicker hyprsunset xdg-desktop-portal-hyprland hyprpolkitagent qt6-wayland qt5-wayland hypridle
 sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd xdg-desktop-portal-gtk pavucontrol base-devel unzip ranger cliphist noto-fonts
+git config --global user.email "beetlunar@pm.me"
+git config --global user.name "beetlunar"
 git clone https://github.com/beetlunar/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
